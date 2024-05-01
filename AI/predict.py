e133de53e951cc458d3c6fce4a86c5ce2680b208
Tyler Pruitt <tylerp@creatrify.com>
2024-05-01 01:06:29 -0700

    Update AI constants and file paths

--- AI/predict.py
+++ AI/predict.py
@@ -6,7 +6,7 @@ from constants import SEQUENCE_LENGTH
 
 # Load in the prediction data
 # Assumes the dataset has columns 'UNIX time', 'open', 'high', 'low', 'close', 'vwap', 'volume', 'count'
-prediction_data = pd.read_csv('AI/crypto_prediction_data.csv')
+prediction_data = pd.read_csv('AI/data/crypto_prediction_data.csv')
 
 scaler = StandardScaler()
 scaled_prediction_data = scaler.fit_transform(prediction_data)
@@ -19,7 +19,7 @@ for i in range(len(scaled_prediction_data) - SEQUENCE_LENGTH):
 X_prediction = np.array(X_prediction)
 
 # Load the trained model
-model = load_model('AI/crypto_price_model.h5')
+model = load_model('AI/models/crypto_price_model.h5')
 
 # Predict using the model
 predictions = model.predict(X_prediction)
