55bdf5bbb549bee6e17cdd131d08a40a2e112b76
Tyler Pruitt <tylerp@creatrify.com>
2026-02-21 00:27:45 +0800

    Refactor logic in main.py

--- main.py
+++ main.py
@@ -17,53 +17,54 @@ if __name__ == '__main__':
         exchange_config = ExchangeConfig()
         strategy_config = StrategyConfig()
         riskmanager_config = RiskManagerConfig()
-
         risk_manager = RiskManager(riskmanager_config)
         
+        # Set exchange
         if exchange_config.exchange_name == 'RobinhoodCrypto':
-            pass
+            raise NotImplementedError
         elif exchange_config.exchange_name == 'RobinhoodOption':
-            pass
+            raise NotImplementedError
         elif exchange_config.exchange_name == 'Kraken':
-            kraken_exchange = KrakenExchange(exchange_config)
-
-            if strategy_config.strategy in ['GRID', 'grid']:
-                # Initialize Kraken gridbot
-                kraken_gridbot = GRIDBot(
-                    gridbot_config=GRIDBotConfig(),
-                    exchange=kraken_exchange
-                )
-                print(kraken_gridbot)
-
-                # Start automated grid trading
-                kraken_gridbot.start()
-            elif strategy_config.strategy in ['GRID_LOAD', 'grid_load']:
-                gridbot_config = GRIDBotConfig()
-
-                # Load Kraken gridbot if it exists
-                kraken_gridbot = GRIDBot.from_json_file(f'app/bots/local/{gridbot_config.name}.json')
-
-                print(kraken_gridbot)
-
-                # Restart automated grid trading
-                kraken_gridbot.restart()
-            elif strategy_config.strategy in ['LSTM', 'lstm']:
-                lstm_strategy = LSTMStrategy(strategy_config, kraken_exchange)
-
-                kraken_lstm_bot = Bot(bot_config, kraken_exchange, lstm_strategy, risk_manager)
-
-                kraken_lstm_bot.run()
-            else:
-                print(f"Strategy {strategy_config.strategy} not valid")
+            exchange = KrakenExchange(exchange_config)
         elif exchange_config.exchange_name == "Coinbase":
-            pass
+            raise NotImplementedError
         elif exchange_config.exchange_name == "Binance_US":
-            pass
+            raise NotImplementedError
         else:
-            print(f"Exchange name {exchange_config.exchange_name} not found")
+            raise ValueError(f"Exchange name {exchange_config.exchange_name} not found")
+        
+        # Set strategy and bot
+        if strategy_config.strategy in ['GRID', 'grid']:
+            # Initialize Kraken gridbot
+            kraken_gridbot = GRIDBot(
+                gridbot_config=GRIDBotConfig(),
+                exchange=exchange
+            )
+            print(kraken_gridbot)
+
+            # Start automated grid trading
+            kraken_gridbot.start()
+        elif strategy_config.strategy in ['GRID_LOAD', 'grid_load']:
+            gridbot_config = GRIDBotConfig()
+
+            # Load Kraken gridbot if it exists
+            kraken_gridbot = GRIDBot.from_json_file(f'app/bots/local/{gridbot_config.name}.json')
+
+            print(kraken_gridbot)
+
+            # Restart automated grid trading
+            kraken_gridbot.restart()
+        elif strategy_config.strategy in ['LSTM', 'lstm']:
+            lstm_strategy = LSTMStrategy(strategy_config, exchange)
+
+            kraken_lstm_bot = Bot(bot_config, exchange, lstm_strategy, risk_manager)
+
+            kraken_lstm_bot.run()
+        else:
+            raise ValueError(f"Strategy {strategy_config.strategy} not valid")
     elif request_config.request in ['LSTM_TRAIN', 'lstm_train']:
         fetch_training_data()
 
         train_model()
     else:
-        print(f"Request {request_config.request} not valid")
+        raise ValueError(f"Request {request_config.request} not valid")
